012ef5943091f8d90c4b28eed29e40d9effa085c
Jonathan Scheer <jonatron@gmail.com>
2025-01-16 14:13:50 -0500

    structure

---
+++ libs/speeracy-cli/src/speeracy_cli/__init__.py
@@ -0,0 +1,9 @@
+from speeracy_core import core_test
+
+
+def main():
+    core_test()
+
+
+if __name__ == "__main__":
+    main()
---
+++ libs/speeracy-core/src/speeracy_core/__init__.py
@@ -0,0 +1,7 @@
+import sys
+
+
+def core_test():
+    # Here a is the array holding the objects
+    # passed as the argument of the function
+    print("hello from core", file=sys.stderr)
